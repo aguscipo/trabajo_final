382ec9774526c8e50a45e71300643a91c52e4de4
aguscipo <agus_cipo_lp@hotmail.com>
2015-08-26 12:11:25 -0300

    Registracion finalizada

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Le damos la bienvenida a AC informática!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params#Cuando se hace el submit del form solo se permiten estos parametros (strong parameters)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
