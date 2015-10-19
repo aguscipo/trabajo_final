575a37ed85ae0339c3b3d87f5e860d7cb0975d7d
aguscipo <agus_cipo_lp@hotmail.com>
2015-10-19 19:28:20 -0300

    Finalizacion manejo de usuarios basico

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update, :show, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :show]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuario eliminado"
+    redirect_to users_url
+  end
+
   def new
     @user = User.new
   end
@@ -19,10 +33,47 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Perfil actualizado"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params#Cuando se hace el submit del form solo se permiten estos parametros (strong parameters)
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Para acceder debe loguearse en AC informática."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
