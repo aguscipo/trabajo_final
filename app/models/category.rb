65091c66349f6874076019879589e066c9458d31
aguscipo <agustincipollone@gmail.com>
2016-07-01 17:59:46 -0300

    category uniqness

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :products
-  validates :name, presence: true, length: { maximum: 100 }
-  def to_param
-  name
-end
+  validates :name, presence: true, length: { maximum: 100 }, uniqueness: true
+
+
+  def to_param #Modificar para que aparezca nombre de categoria en la url
+    name
+  end
 end
